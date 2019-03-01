7683fd45b8d2a4f650a4953380db8a0e6d212275
guoxinyu <guoxinyu@qq.com>
2019-03-01 15:23:36 +0800

    社区页面fragment

---
+++ community/src/main/java/com/wd/community/fragment/CommunityFragment.java
@@ -0,0 +1,26 @@
+package com.wd.community.fragment;
+
+import com.wd.common.base.fragment.BaseNetFragment;
+import com.wd.community.R;
+
+public class CommunityFragment extends BaseNetFragment {
+    @Override
+    protected void netSuccess(Object object) {
+
+    }
+
+    @Override
+    protected void netFail(String s) {
+
+    }
+
+    @Override
+    protected int setView() {
+        return R.layout.community_fragment;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
