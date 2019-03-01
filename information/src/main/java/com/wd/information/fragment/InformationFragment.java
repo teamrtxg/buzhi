9d7186c1fe9f32f7a16fd60b64370daf9aa08fed
Administrator <928873848@qq.com>
2019-03-01 19:34:14 +0800

    home页面

--- information/src/main/java/com/wd/information/fragment/InformationFragment.java
+++ information/src/main/java/com/wd/information/fragment/InformationFragment.java
@@ -1,9 +1,14 @@
-package com.wd.main.fragment;
+package com.wd.information.fragment;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.wd.common.base.fragment.BaseNetFragment;
-import com.wd.main.R;
-@Route(path = "/main/InformationFragment")
+import com.wd.information.R;
+/**
+ *资讯页
+ *@author Administrator
+ *@time 2019/3/1 0001 17:04
+ */
+@Route(path = "/information/InformationFragment")
 public class InformationFragment extends BaseNetFragment {
     @Override
     protected void netSuccess(Object object) {
