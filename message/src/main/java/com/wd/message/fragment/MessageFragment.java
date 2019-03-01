fb87fa83aeca89f9122b7b00af2381131fd39433
xieyu <2814874857@qq.com>
2019-03-01 15:55:34 +0800

    消息页面

---
+++ message/src/main/java/com/wd/message/fragment/MessageFragment.java
@@ -0,0 +1,23 @@
+package com.wd.message.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.wd.common.base.fragment.BaseFragment;
+
+public class MessageFragment extends BaseFragment {
+    @Override
+    protected int setView() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
